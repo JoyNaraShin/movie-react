862b6e52d7ea75e5acb1840928fe4a2b41956a39
JoyNaraShin <joyshin93@gmail.com>
2020-11-19 11:10:40 +0900

    lint&prettier 환경설정 추가, page 완성

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import React from 'react';
 import axios from 'axios';
-import Movie from './Movies'
+import Movie from './Movies';
+import './App.css';
 
-class App extends React.Component{
+class App extends React.Component {
   state = {
-    isLoading : true,
+    isLoading: true,
     movies: [],
   };
-  getMovies = async () => {    
-    const {data:{data:{movies}}} = await axios.get('https://yts-proxy.nomadcoders1.now.sh/list_movies.json');
-    this.setState({movies, isLoading:false});
-    console.log(movies)
-  }
+
   async componentDidMount() {
     this.getMovies();
   }
+
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      'https://yts-proxy.nomadcoders1.now.sh/list_movies.json',
+    );
+    this.setState({ movies, isLoading: false });
+  };
+
   render() {
-    const {isLoading, movies} = this.state;
-    return <div>{isLoading ? 'Loading...' : movies.map(item=>{
-      return <Movie id={item.id} year={item.year} title={item.title} summury={item.summary} poster={item.poster} />
-    })}</div>
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(item => (
+              <Movie
+                key={item.id}
+                year={item.year}
+                title={item.title}
+                summary={item.summary}
+                poster={item.medium_cover_image}
+                genres={item.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
   }
 }
 
